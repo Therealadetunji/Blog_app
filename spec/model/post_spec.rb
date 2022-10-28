7c521fc18e87adeb2841f71bf25c01ac848fbd75
Michael <madetunji1376@gmail.com>
2022-10-29 00:22:10 +0100

    validations and model specs

---
+++ spec/model/post_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  # tests go here
+  user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+                     postcounter: 1)
+  post = Post.new(author: user, title: 'Half of a yellow sun', text: 'This is my first post', commentscounter: 1,
+                  likescounter: 2)
+
+  before { post.save }
+
+  it 'title must be present' do
+    post.title = 'Half of a yellow sun'
+    expect(post).to be_valid
+  end
+
+  it 'post shouldnt be more than 250 words' do
+    post.title = 'Half of a yellow sun'
+    expect(post).to be_valid
+  end
+
+  it 'comments counter test' do
+    post.commentscounter = 1
+    expect(post).to be_valid
+  end
+
+  it 'likes counter test' do
+    post.likescounter = 2
+    expect(post).to be_valid
+  end
+end
