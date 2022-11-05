bb975ace2094501999a6a083d9feccecc4590af3
Michael <madetunji1376@gmail.com>
2022-11-05 20:13:23 +0100

    reviwer suggestions done

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,26 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"
   get "/users/:id/posts/:id", to: "posts#show"
+  get "/users/:user_id/posts/new", to: "posts#new"
+  get "/users/:user_id/posts/:post_id/comments/new", to: "comments#new"
 
-  resources :users do
-    resources :posts
-  end
+  
+  post "/users/:user_id/posts", to: "posts#create"
+  post "/users/:user_id/posts/:post_id/likes", to: "likes#create"
+  post "/users/:user_id/posts/:post_id/comments", to: "comments#create"
 
   root "users#index"
+
+
+  
+
+  resources :users do
+    resources :posts do
+      resources :likes
+      resources :comments
+    end
+  end
+
 
 
   
