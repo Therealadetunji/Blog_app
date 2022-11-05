71e86e62a6231751f061b3c79c58e57680839278
Michael <madetunji1376@gmail.com>
2022-11-05 18:12:58 +0100

    forms created and likes and comments controller created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,3 +8,24 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 end
+
+def new
+  @post = Post.new
+end
+
+def create
+  @post = current_user.posts.new(post_param[:id])
+  respond_to do |format|
+    format.html do
+      if @post.save
+        redirect_to user_post_path(current_user, @post), notice: 'Post was created.'
+      else
+        render :new, status: 'Error: Post was not created.'
+      end
+    end
+  end
+end
+
+def post_param
+  params.require(:post).permit(:title, :text)
+end
